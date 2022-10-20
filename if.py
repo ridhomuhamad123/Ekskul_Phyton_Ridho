d321ac0d01cebd685d7944e21105f29548a713a9
atjeprahmat <atjeprahmat@gmail.com>
2022-10-20 17:08:51 +0700

    tambah_baru

--- if.py
+++ if.py
@@ -1,7 +1,7 @@
 nilai = 9
 
 if(nilai > 7):
-    print("Sembilan Lebih Besar Dari Angka Tujuh")
+    print(nilai, "lebih besar dari pada", 7)
 
 if(nilai > 10):
     print("Sembilan Lebih Besar Dari Angka Sepuluh")
